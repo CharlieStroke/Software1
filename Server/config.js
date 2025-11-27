18bdbec6d358463de6a4b1e2e6fe42ed4fd39702
jb340 <174159972+jb340@users.noreply.github.com>
2025-11-27 12:22:59 -0600

    config fix

--- Server/config.js
+++ Server/config.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 // Cargar variables de entorno
 dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.MYSQLHOST || 'localhost';
-export const DB_PORT = process.env.MYSQLPORT || 3306;
-export const DB_USER = process.env.MYSQLUSER || 'root';
-export const DB_PASSWORD = process.env.MYSQLPASSWORD || '';
-export const DB_NAME = process.env.MYSQLDATABASE || 'restaurante_db';
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'secret_key_default';
-export const URLPERMITED = process.env.URLPERMITED || 'http://localhost:5173';
+export const PORT = process.env.PORT;
+export const DB_HOST = process.env.MYSQLHOST;
+export const DB_PORT = process.env.MYSQLPORT;
+export const DB_USER = process.env.MYSQLUSER;
+export const DB_PASSWORD = process.env.MYSQLPASSWORD;
+export const DB_NAME = process.env.MYSQLDATABASE;
+export const TOKEN_SECRET = process.env.TOKEN_SECRET;
+export const URLPERMITED = process.env.URLPERMITED;
 export const SESSION_TIMEOUT = process.env.SESSION_TIMEOUT || 3600;
